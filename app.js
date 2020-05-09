ac1b8a96a8a7ea1b08f7819fde78988efae41eb3
Matin Misaghi <matinmisaghi@gmail.com>
2020-05-08 23:02:32 -0700

    add the audio in. Press the play button and loop through the tracks. Give the active squares animation and loop the animation to go back to the first square after each loop.

--- app.js
+++ app.js
@@ -9,20 +9,39 @@ class Drumkit {
         this.bpm = 150;
     }
     activePad (){
-        console.log(this);
         this.classList.toggle('active');
     }
     repeat (){
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        // Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            // Check if pads are active
+            if (bar.classList.contains('active')) {
+                // Check each sound
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start () {
         const interval = (60/this.bpm) * 1000;
+        // Check if it is playing
         setInterval(() => {
-            this.repeat();
-        }, interval);
+                this.repeat();
+            }, interval);
     }
 }
 
@@ -30,6 +49,9 @@ const drumkit = new Drumkit();
 
 drumkit.pads.forEach(pad => {
     pad.addEventListener('click', drumkit.activePad);
+    pad.addEventListener('animationend', function() {
+        this.style.animation = "";
+    });
 });
 
 drumkit.playBtn.addEventListener('click', () => { 
